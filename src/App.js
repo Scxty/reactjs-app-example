f57cbe6687f5979bb4e0400407dc59086ab78c24
Wilfried Kakou <mianquihin15@gmail.com>
2019-05-11 21:10:11 +0000

    tuto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,141 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            firstName: "",
+            lastName: "",
+            age: null,
+            gender: "",
+            destination: "",
+            isVegan: false,
+            isKosher: false,
+            isLactoseFree: false
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(event) {
+        const { name, value, type, checked } = event.target
+        type === "checkbox" ?
+            this.setState({
+                [name]: checked
+            })
+            :
+            this.setState({
+                [name]: value
+            })
+    }
+    render() {
+        return (
+            <main>
+                <form>
+                    <input
+                        name="firstName"
+                        value={this.state.firstName}
+                        onChange={this.handleChange}
+                        placeholder="first Name"
+                    />
+                    <br />
+                    <input
+                        name="lastName"
+                        value={this.state.lastName}
+                        onChange={this.handleChange}
+                        placeholder="last Name" /
+                    >
+                    <br />
+
+                    <input
+                        name="age"
+                        value={this.state.age}
+                        onChange={this.handleChange}
+                        placeholder="age" /><br /
+                    >
+                    <br />
+
+                    <label>
+                        <input
+                            type="radio"
+                            name="gender"
+                            value="male"
+                            checked={this.state.gender === "male"}
+                            onChange={this.handleChange}
+                        /> male
+                    </label>
+                    <br />
+                    <br />
+                    <label>
+                        <input
+                            type="radio"
+                            name="gender"
+                            value="female"
+                            checked={this.state.gender === "female"}
+                            onChange={this.handleChange}
+                        /> female
+                    </label>
+                    <br />
+                    <br />
+                    <select
+                        value={this.state.destination}
+                        name="destination"
+                        onChange={this.handleChange}
+                    >
+                        <option value="">--- Please Choose a destination ---</option>
+                        <option value="germany">Germany</option>
+                        <option value="norway">Norway</option>
+                        <option value="north-pole">North Pole</option>
+                        <option value="south-pole">South Pole</option>
+                    </select>
+                    <br />
+                    <br />
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="isVegan"
+                            onChange={this.handleChange}
+                            checked={this.state.isVegan}
+                        />Vegan ?
+                    </label>
+                    <br />
+                    <br />
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="isKosher"
+                            onChange={this.handleChange}
+                            checked={this.state.isKosher}
+                        />Kosher ?
+                    </label>
+                    <br />
+                    <br />
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="isLactoseFree"
+                            onChange={this.handleChange}
+                            checked={this.state.isLactoseFree}
+                        />Lactose Free ?
+                    </label>
+                    <br />
+                    <br />
+                    <button>Submit</button>
+                </form>
+                <hr />
+                <h1>Your Billing</h1>
+                <p>Your name: {this.state.firstName}  {this.state.lastName}</p>
+                <p>Your age: {this.state.age}  </p>
+                <p>Your gender: {this.state.gender} </p>
+                <p>Your destination: {this.state.destination} </p>
+                <p>Your dietary restrictions: <br/>
+                Vegan: {this.state.isVegan ? "Yes" : "No"} <br/>
+                Kosher: {this.state.isKosher ? "Yes" : "No"} <br/>
+                Lactose Free: {this.state.isLactoseFree ? "Yes" : "No"} <br/>
+                </p>
+            </main>
+        )
+    }
 }
 
 export default App;
